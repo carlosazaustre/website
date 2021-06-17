f59c0b165a8dcdaa417a02045e54ede48aa39fcb
Carlos Azaustre <cazaustre@gmail.com>
2021-06-17 19:10:48 +0200

    fix: :ambulance: re-re-fix google fonts using fontsource

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import "@/styles/prism-night-owl.css";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/styles/theme";
-import "@/styles/prism-night-owl.css";
 
 function MyApp({ Component, pageProps }) {
   return (
