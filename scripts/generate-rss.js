917700cd020698ecc0a1133c50460b4392b2c61f
Carlos Azaustre <cazaustre@gmail.com>
2021-06-17 06:16:01 +0200

    generate sitemap

--- scripts/generate-rss.js
+++ scripts/generate-rss.js
@@ -1,13 +1,18 @@
-const { promises: fs } = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const RSS = require("rss");
 const matter = require("gray-matter");
 
-async function generate() {
+(async () => {
   const feed = new RSS({
     title: "Carlos Azaustre - Desarrollo y Formación JavaScript",
+    description:
+      "Tutoriales de Programación y desarrollo web con JavaScript, React, Node.js y más",
     site_url: "https://carlosazaustre.es",
     feed_url: "https://carlosazaustre.es/rss.xml",
+    webMaster: "Carlos Azaustre",
+    copyright: `2011-${new Date().getFullYear} Carlos Azaustre`,
+    language: "es",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "data", "posts"));
@@ -26,12 +31,14 @@ async function generate() {
       if (frontmatter.data?.summary) {
         item.description = frontmatter.data.summary;
       }
+      if (frontmatter.data?.tags) {
+        item.categories = frontmatter.data.tags;
+      }
 
       feed.item(item);
     })
   );
 
   await fs.writeFile("./public/rss.xml", feed.xml({ indent: true }));
-}
-
-generate();
+  console.log(`🚀 RSS Genearated for ${posts.length} posts`);
+})();
