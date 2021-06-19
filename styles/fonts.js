d0723a56006b532c182c952651d922b188df7500
Carlos Azaustre <cazaustre@gmail.com>
2021-06-19 18:56:46 +0200

    compose theme provider and fonts

---
+++ styles/fonts.js
@@ -0,0 +1,8 @@
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
