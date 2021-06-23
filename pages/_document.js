fb3315322c632dc1fe2774113dcf340def4d9663
Carlos Azaustre <cazaustre@gmail.com>
2021-06-23 21:56:15 +0200

    :bug: hotfix: error on ga script

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <scrip
+          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
