42b9453742bcaf1e317669788df31353165a134b
Carlos Azaustre <cazaustre@gmail.com>
2021-06-24 08:11:49 +0200

    :zap: use pagination on blog section(#47)

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,57 @@
+import { useRef, useEffect, useState } from 'react';
 import NextLink from "next/link";
 
 import { Layout, PostListItem } from "@/components";
 import { formatDate } from "@/lib/format-date";
 import { orderByDate } from "@/lib/order-by-date";
 import { getAllFilesFrontMatter } from "@/lib/mdx";
+import { usePagination } from '@/lib/use-pagination';
 
 export default function Blog({ posts }) {
+  const { next, currentPage, currentData, maxPage } = usePagination(posts, 5);
+  const [element, setElement] = useState(null);
+  const observer = useRef();
+  const prevY = useRef(0);
+
+  const currentPosts = currentData();
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        const firstEntry = entries[0];
+        const y = firstEntry.boundingClientRect.y;
+
+        if (prevY.current > y) {
+          next();
+        }
+        prevY.current = y;
+      },
+      { threshold: 0.5 }
+    );
+  }, []);
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+  
   const metadata = {
     title: "Últimos Artículos",
   };
 
   return (
     <Layout type="post" metadata={metadata}>
-      {posts.map((post) => (
+      {currentPosts && currentPosts.map((post) => (
         <NextLink href={post.slug} key={post.slug}>
           <a>
             <PostListItem
@@ -23,6 +62,7 @@ export default function Blog({ posts }) {
           </a>
         </NextLink>
       ))}
+      {currentPage !== maxPage && (<div ref={setElement}>Cargando...</div>)}
     </Layout>
   );
 }
