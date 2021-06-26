e524ab3730d3e58eae34be9ee72a301a21233d66
Carlos Azaustre <cazaustre@gmail.com>
2021-06-26 09:19:10 +0200

    get latest videos from Youtube via script

---
+++ scripts/generate-video-data.js
@@ -0,0 +1,26 @@
+const fs = require("fs/promises");
+const path = require("path");
+const fetch = require("node-fetch");
+
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const YOUTUBE_API_URL = `https://www.googleapis.com/youtube/v3`;
+const YOUTUBE_CHANNEL_PLAYLIST = `UUJgGc8pQO1lv04VXrBxA_Hg`;
+const NUMBER_OF_VIDEOS = 5;
+
+const DATA_FILE = path.join(__dirname, "..", "data", "latestVideos.json");
+
+const getLatestYoutubeVideos = async () => {
+  const queryEndpoint = `/playlistItems?part=snippet&playlistId=${YOUTUBE_CHANNEL_PLAYLIST}&maxResults=${NUMBER_OF_VIDEOS}`;
+
+  const response = await fetch(
+    `${YOUTUBE_API_URL}${queryEndpoint}&key=${YOUTUBE_API_KEY}`
+  );
+  const data = await response.json();
+  return data.items;
+};
+
+(async () => {
+  const videos = await getLatestYoutubeVideos();
+  const data = JSON.stringify(videos);
+  await fs.writeFile(DATA_FILE, data);
+})();
