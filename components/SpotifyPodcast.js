f04a8758d6e9150c8e062f891d2f667851213d73
Carlos Azaustre <cazaustre@gmail.com>
2021-06-27 16:18:55 +0200

    refactor

--- components/SpotifyPodcast.js
+++ components/SpotifyPodcast.js
@@ -3,6 +3,11 @@ const SIZES = {
   compact: "152",
 };
 
+const THEMES = {
+  light: "?t=0",
+  dark: "?theme=0&amp;t=0",
+};
+
 export const SpotifyPodcast = ({
   episode = "52KMznI6l9x4MoZrO4uIy0",
   theme = "light",
@@ -10,9 +15,7 @@ export const SpotifyPodcast = ({
 }) => {
   return (
     <iframe
-      src={`https://open.spotify.com/embed/episode/${episode}?${
-        theme === "dark" && "theme=0&amp;"
-      }t=0`}
+      src={`https://open.spotify.com/embed/episode/${episode}${THEMES[theme]}`}
       width="100%"
       height={SIZES[size]}
       frameBorder="0"
