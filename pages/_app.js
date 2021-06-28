bdb5ab01584276c6812102eeca5555f65c17583a
Carlos Azaustre <cazaustre@gmail.com>
2021-06-28 11:37:33 +0200

    fix: :ambulance: Delete browser cache by Gatsby Plugin

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,38 @@ function MyApp({ Component, pageProps, err }) {
   const router = useRouter();
 
   useEffect(() => {
+    // Temporary solution to delecte old cached site
+    const DBDeleteRequest = window.indexedDB.deleteDatabase(
+      "gatsby-plugin-offline-precache-https___carlosazaustre_es_"
+    );
+
+    DBDeleteRequest.onerror = function (event) {
+      console.log("Error deleting database.");
+    };
+
+    DBDeleteRequest.onsuccess = function (event) {
+      console.log("Database deleted successfully");
+
+      console.log(event.result); // should be undefined
+    };
+  }, []);
+
+  useEffect(() => {
+    // Temporary solution to delecte old cached site
+    const DBDeleteRequest = window.indexedDB.deleteDatabase("keyval-store");
+
+    DBDeleteRequest.onerror = function (event) {
+      console.log("Error deleting database.");
+    };
+
+    DBDeleteRequest.onsuccess = function (event) {
+      console.log("Database deleted successfully");
+
+      console.log(event.result); // should be undefined
+    };
+  }, []);
+
+  useEffect(() => {
     const handleRouteChange = (url) => {
       ga.pageView(url);
     };
