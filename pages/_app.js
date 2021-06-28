c1610508dad7c8a5af420110ea4615952f7bb049
Carlos Azaustre <cazaustre@gmail.com>
2021-06-28 13:30:29 +0200

    fix errors

--- pages/_app.js
+++ pages/_app.js
@@ -7,39 +7,13 @@ import "@/styles/fonts";
 import theme, { ThemeProvider } from "@/styles/theme";
 
 function MyApp({ Component, pageProps, err }) {
+  if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+    window.navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((r) => r.unregister());
+    });
+  }
+
   const router = useRouter();
-
-  useEffect(() => {
-    // Temporary solution to delecte old cached site
-    const DBDeleteRequest = window.indexedDB.deleteDatabase(
-      "gatsby-plugin-offline-precache-https___carlosazaustre_es_"
-    );
-
-    DBDeleteRequest.onerror = function (event) {
-      console.log("Error deleting database.");
-    };
-
-    DBDeleteRequest.onsuccess = function (event) {
-      console.log("Database deleted successfully");
-
-      console.log(event.result); // should be undefined
-    };
-  }, []);
-
-  useEffect(() => {
-    // Temporary solution to delecte old cached site
-    const DBDeleteRequest = window.indexedDB.deleteDatabase("keyval-store");
-
-    DBDeleteRequest.onerror = function (event) {
-      console.log("Error deleting database.");
-    };
-
-    DBDeleteRequest.onsuccess = function (event) {
-      console.log("Database deleted successfully");
-
-      console.log(event.result); // should be undefined
-    };
-  }, []);
 
   useEffect(() => {
     const handleRouteChange = (url) => {
