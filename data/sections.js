bb694e0986c0bd516536432304f473b7cf43d235
Carlos Azaustre <cazaustre@gmail.com>
2021-07-02 08:06:18 +0200

    :speech_balloon: feat: add discord link

--- data/sections.js
+++ data/sections.js
@@ -1,17 +1,9 @@
 const sections = [
-  {
-    name: "Blog",
-    url: "/blog",
-  },
-  {
-    name: "About",
-    url: "/p/sobre-mi",
-  },
-  {
-    name: "Mentoría",
-    url: "/p/mentoria",
-  },
+  { name: "Blog", url: "/blog" },
+  { name: "Mentoría", url: "/p/mentoria" },
+  { name: "Discord", url: "https://bit.ly/pintaapis" },
   { name: "Cursos", url: "/cursos" },
+  { name: "About", url: "/p/sobre-mi" },
 ];
 
 export default sections;
