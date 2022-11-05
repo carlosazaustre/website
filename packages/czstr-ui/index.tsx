b279ef0cf441c1f9f979ede53a1691e87061e0bf
Carlos Azaustre <cazaustre@gmail.com>
2022-11-05 14:35:04 +0100

    refactor(ui): :fire: remove unused component

--- packages/czstr-ui/index.tsx
+++ packages/czstr-ui/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 export { ThemeProvider, theme, createLocalStorageManager } from "./src/theme";
-export { Button, type ButtonProps } from "./src/Button";
 export { YouTube } from "./src/YouTube";
 export { YouTubeBtn } from "./src/YouTubeBtn";
 export { TwitterShare } from "./src/TwitterShare";
