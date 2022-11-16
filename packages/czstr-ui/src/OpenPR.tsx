db8ae6b5fc0f8fbc2a7a6c27a1b24e45c87dc6ca
Carlos Azaustre <cazaustre@gmail.com>
2022-11-16 10:01:35 +0100

    Update OpenPR.tsx

--- packages/czstr-ui/src/OpenPR.tsx
+++ packages/czstr-ui/src/OpenPR.tsx
@@ -23,7 +23,7 @@ export const OpenPR = ({ slug }: { slug: string}) => {
             color="brand.900"
             target="_blank"
             rel="noopener noreferrer"
-            href={`https://github.com/${github.user}/${github.repo}/edit/main/data/posts/${slug}.mdx`}
+            href={`https://github.com/${github.user}/${github.repo}/edit/main/apps/web/data/posts/${slug}.mdx`}
         >
           Haz una Pull Request{" "}
           <ExternalLinkIcon mx="2px" />
