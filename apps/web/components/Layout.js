c6a3035d4801dd87c565968295b47962fd7138b6
Carlos Azaustre <cazaustre@gmail.com>
2023-09-29 12:13:57 +0200

    refactor(web): :fire: update text and remove unused components

--- apps/web/components/Layout.js
+++ apps/web/components/Layout.js
@@ -7,56 +7,51 @@ import { PostMetadata } from "./PostMetadata";
 import { Disqus } from "./Disqus";
 
 export const Layout = ({ children, metadata = {}, type = "post" }) => {
-  const isBlogTemplate = type === "post" && metadata.date;
+	const isBlogTemplate = type === "post" && metadata.date;
 
-  return (
-    <UI.Box
-      bg="secondary.900"
-      borderTop="8px solid"
-      borderColor="brand.900"
-      color="white"
-    >
-      <OpenGraph metadata={metadata} />
-      <UI.Container maxW="container.md" pt="8">
-        <SectionLinks />
+	return (
+		<UI.Box
+			bg="secondary.900"
+			borderTop="8px solid"
+			borderColor="brand.900"
+			color="white"
+		>
+			<OpenGraph metadata={metadata} />
+			<UI.Container maxW="container.md" pt="8">
+				<SectionLinks />
 
-        <UI.Flex direction="column">
-          <UI.Box as="article" my={4} width="100%">
-            {isBlogTemplate ? (
-              <Fragment>
-                <UI.Heading
-                  as="h1"
-                  size="xl"
-                  mt={8}
-                  mb={4}
-                  fontFamily="heading"
-                >
-                  {metadata.title}
-                </UI.Heading>
-                <PostMetadata metadata={metadata} />
-              </Fragment>
-            ) : (
-              <UI.Heading as="h2" my={8} size="xl">
-                {metadata.title}
-              </UI.Heading>
-            )}
-            <UI.Box m="0" as="section">
-              {children}
-            </UI.Box>
-            {isBlogTemplate && (
-              <aside id="comments">
-                <Disqus title={metadata.title} slug={metadata.slug} />
-              </aside>
-            )}
-          </UI.Box>
-          {type === "post" && (
-            <UI.Stack as="aside" spacing="24px" my="4" width="100%">
-              <NewsletterForm />
-            </UI.Stack>
-          )}
-        </UI.Flex>
-      </UI.Container>
-      <Footer />
-    </UI.Box>
-  );
+				<UI.Flex direction="column">
+					<UI.Box as="article" my={4} width="100%">
+						{isBlogTemplate ? (
+							<Fragment>
+								<UI.Heading
+									as="h1"
+									size="xl"
+									mt={8}
+									mb={4}
+									fontFamily="heading"
+								>
+									{metadata.title}
+								</UI.Heading>
+								<PostMetadata metadata={metadata} />
+							</Fragment>
+						) : (
+							<UI.Heading as="h2" my={8} size="xl">
+								{metadata.title}
+							</UI.Heading>
+						)}
+						<UI.Box m="0" as="section">
+							{children}
+						</UI.Box>
+						{isBlogTemplate && (
+							<aside id="comments">
+								<Disqus title={metadata.title} slug={metadata.slug} />
+							</aside>
+						)}
+					</UI.Box>
+				</UI.Flex>
+			</UI.Container>
+			<Footer />
+		</UI.Box>
+	);
 };
